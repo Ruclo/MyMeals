bf76acc6e2507f095a8f87c897195b5416b74184
Ruclo <vavrinec.misko@gmail.com>
2025-03-13 09:15:15 +0100

    db init logic change

--- internal/database/db.go
+++ internal/database/db.go
@@ -9,7 +9,7 @@ import (
 	"os"
 )
 
-func GetEnvOrExit(key string) string {
+func getEnvOrExit(key string) string {
 	value, exists := os.LookupEnv(key)
 	if !exists {
 		log.Fatal(key + " env variable not found")
@@ -17,22 +17,24 @@ func GetEnvOrExit(key string) string {
 	return value
 }
 
-func InitDB() *gorm.DB {
-	log.Println("Starting DB Initialization")
-
-	host := GetEnvOrExit("DB_HOST")
-	user := GetEnvOrExit("DB_USER")
-	password := GetEnvOrExit("DB_PASSWORD")
-	name := GetEnvOrExit("DB_NAME")
-	port := GetEnvOrExit("DB_PORT")
+func createConnection() *gorm.DB {
+	host := getEnvOrExit("DB_HOST")
+	user := getEnvOrExit("DB_USER")
+	password := getEnvOrExit("DB_PASSWORD")
+	name := getEnvOrExit("DB_NAME")
+	port := getEnvOrExit("DB_PORT")
 
 	dbString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, name, port)
 	db, err := gorm.Open(postgres.Open(dbString), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the DB: ", err)
 	}
-	
-	err = db.SetupJoinTable(&models.Order{}, "Meals", &models.OrderMeal{})
+
+	return db
+}
+
+func migrateSchema(db *gorm.DB) {
+	err := db.SetupJoinTable(&models.Order{}, "Meals", &models.OrderMeal{})
 	if err != nil {
 		log.Fatal("Setting up the join table for orders and meals failed: ", err)
 	}
@@ -40,6 +42,30 @@ func InitDB() *gorm.DB {
 	if err != nil {
 		log.Fatal("Schema migration failed: ", err)
 	}
+}
 
+func wipeDB(db *gorm.DB) {
+	err := db.Migrator().DropTable(&models.OrderMeal{}, &models.Review{}, &models.Order{}, &models.Meal{}, &models.StaffMember{})
+	if err != nil {
+		log.Fatal("Failed to wipe the DB: ", err)
+	}
+}
+
+func InitDB() *gorm.DB {
+	log.Println("Starting DB Initialization")
+
+	db := createConnection()
+	migrateSchema(db)
+
+	return db
+}
+
+func InitTestDB() *gorm.DB {
+	log.Println("Starting DB Initialization")
+	log.Println("Wiping DB")
+
+	db := createConnection()
+	wipeDB(db)
+	migrateSchema(db)
 	return db
 }
---
+++ internal/repositories/meal_repository_test.go
@@ -0,0 +1,15 @@
+package repositories
+
+import "testing"
+
+func TestMealRepository_Create(t *testing.T) {
+
+}
+
+func TestMealRepository_Update(t *testing.T) {
+
+}
+
+func TestMealRepository_GetAll(t *testing.T) {
+
+}
